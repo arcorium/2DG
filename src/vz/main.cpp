fc73fffc4926efedffdece6a77544a3bad24d982
vizharc <mizzhan.ar@gmail.com>
2022-02-03 09:47:07 +0700

    Add needed stb thirdparty and precompiled header

---
+++ src/vz/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by vizharc on 03/02/22.
+//
+
+#include <vzpch.h>
+
+int main()
+{
+    std::cout << "TEST" << std::endl;
+    return 0;
+}
---
+++ src/vzpch.h
@@ -0,0 +1,16 @@
+//
+// Created by vizharc on 03/02/22.
+//
+
+#ifndef INC_2DG_VZPCH_H
+#define INC_2DG_VZPCH_H
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#endif //INC_2DG_VZPCH_H
